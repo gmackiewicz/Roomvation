d0a45d2585450a57f881b0c0eb6d47f5ecad42f6
Gabriel Mackiewicz <gabriel.mackiewicz@gmail.com>
2017-03-20 11:11:22 +0100

    Clean project with migrations enabled

---
+++ Roomvation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Roomvation.MvcApplication" Language="C#" %>
---
+++ Roomvation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Roomvation.Startup))]
+namespace Roomvation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
